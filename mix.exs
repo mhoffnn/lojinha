66f4a41c0eaf6a63f0020a5c900bc2c9e93fe3fd
mhoffnn <matheushofmar@hotmail.com>
2022-05-11 14:43:58 -0300

    Add status readme

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lojinha.MixProject do
     [
       app: :lojinha,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
